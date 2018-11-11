794a7ae97a7d1e965e6b5d38fffbaedd9742d484
mandja96 <mandja96@gmail.com>
2018-11-11 02:42:20 +0100

    Eksperimentisanje sa LookAt i Perspective

--- lonelyroom.c
+++ lonelyroom.c
@@ -6,12 +6,17 @@
 #include <stdio.h>
 #include <GL/glut.h>
 
-/* Ova funkcija vrsi OpenGL inicijalizaciju */
+/* Dimenzije prozora */
+static int window_width, window_height;
+
+
+/* OpenGL inicijalizacija */
 static void initialize(void); 
 
 /* Inicijalizacija callback funkcija */
 static void on_display(void);
 static void on_keyboard(unsigned char key, int x, int y);
+static void on_reshape(int width, int height);
 
 int main(int argc, char **argv){
 
@@ -29,6 +34,7 @@ int main(int argc, char **argv){
 	/* Registracija callback funkcija za obradu dogadjaja*/
 	glutDisplayFunc(on_display);
 	glutKeyboardFunc(on_keyboard);
+	glutReshapeFunc(on_reshape);
 
 	/* Inicijalizacija OpenGL */
 	initialize();
@@ -39,17 +45,70 @@ int main(int argc, char **argv){
 	exit(EXIT_SUCCESS);
 }
 
+static void on_reshape(int width, int height){
+	/* Pamcenje visine i sirine prozora */	
+	window_width = width;
+	window_height = height;	
+}
+
 static void on_display(void){
-	/* Brisemo stari prozor */
+	/* Brisanje starog prozora */
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	/* Saljemo novu sliku na ekran */
+	/* Namestanje viewport */
+	glViewport(0, 0, window_width, window_height);
+	
+	/* Namestanje projekcije */
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(
+		60, 
+		window_width/(float)window_height,
+		0, 50
+	);
+
+	/* Namestanje tacke pogleda */
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(
+		6, 6, 4,
+		0, 0, 0,
+		0, 1, 0 
+	);
+
+	glPushMatrix();
+	
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	glBegin(GL_POLYGON);
+		glColor3f(0.5, 0.5, 1);
+		glVertex3f(0, 3, 1.5);
+        glVertex3f(0, 3, 2.5);
+		glVertex3f(0, 2, 2.5);
+		glVertex3f(0, 2, 1.5);
+    glEnd();
+
+	glPopMatrix();
+
+	glBegin(GL_LINES);
+		glColor3f(1, 1, 1);
+
+		glVertex3f(0, 0, 0);
+		glVertex3f(10, 0, 0);
+
+		glVertex3f(0, 0, 0);
+		glVertex3f(0, 10, 0);
+	
+		glVertex3f(0, 0, 0);
+		glVertex3f(0, 0, 10);
+	glEnd();
+
+	/* Slanje novog sadrzaja na ekran */
 	glutSwapBuffers();
 }
 
 static void on_keyboard(unsigned char key, int x, int y){
 	switch(key){
-		/* Ovaj slucaj je sa ESC: 27, koji izlazi iz programa */ 
+		/* Izlaz iz programa za ESC = 27 */ 
 		case 27:		
 			exit(0);
 			break;
@@ -59,14 +118,20 @@ static void on_keyboard(unsigned char key, int x, int y){
 }
 
 static void initialize(void){
-	/* Postavljamo boju pozadine prozora */
+	/* Boja pozadine prozora */
 	glClearColor(0.2, 0.2, 0.2, 0);
 
-	/* Dobijamo trenutnu debljinu tacke */
+	/* Trenutna debljina tacke */
 	GLfloat point_size;
 	glGetFloatv(GL_POINT_SIZE, &point_size);
-	printf("%g\n", point_size);
 
-	/* Menjamo debljinu tacke */
-	glPointSize(2);
+	/* Promena debljine tacke */
+	glPointSize(6);
+
+	/* Promena debljine linije */
+	glLineWidth(50); 
+	
+	/* Podesavanje sablona na koji ce se iscrtavati linije */
+    glEnable(GL_LINE_STIPPLE);
+	glLineStipple(1, 0x0101); 
 }
